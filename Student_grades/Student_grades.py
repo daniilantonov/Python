e6918e61d1d1626a50ec38aee7c009003378584a
Danil <daniilantonov>
2023-03-01 17:41:21 +0200

    new

--- Student_grades/Student_grades.py
+++ Student_grades/Student_grades.py
@@ -1,19 +1,19 @@
 def reading_data(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
-        dict = {}
+        my_dict = {}
         for line in file:
             name, value = line.split(',')
-            dict[name] = dict.get(name, []) + [int(value.rstrip('\n'))]
-
-    return dict
-
-def save_result(dict):
-    for key, value in dict.items():
-        mini = min(value)
-        sred = sum(value) / len(value)
-        maxi = max(value)
-    with open('output.txt', 'a+', encoding='utf-8') as file:
-            file.write(f'{key}-{mini}, {sred}, {maxi} \n')
+            my_dict[name] = my_dict.get(name, []) + [int(value.rstrip('\n'))]
+    return my_dict
 
 
+def save_result(my_dict):
+    with open('output.txt', 'w', encoding='utf-8') as file:
+        for key, value in my_dict.items():
+            mini = min(value)
+            average = sum(value) / len(value)
+            maxi = max(value)
+            file.write(f'{key}-{mini}, {average}, {maxi}\n')
+            
+            
 save_result(reading_data('input.txt'))
