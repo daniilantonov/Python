4eeaf8e13178af77be62fe54b115fc8be47f6834
Danil <daniilantonov>
2023-03-03 20:11:50 +0200

    new

---
+++ Student_grades/script.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+
+def reading_student_grades(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        student_grades = defaultdict(list)
+        for line in file:
+            value = line.strip().split(',')
+            if len(value) != 2:
+                raise Exception('File structure not valid')
+            name = value[0]
+            grade = int(value[1])
+            student_grades[name].append(grade)
+    return student_grades
+
+def save_accumulated_grades(student_grades , file_name):
+    with open('output.txt', 'w', encoding='utf-8') as file:
+        for student_name, grades in student_grades.items():
+            min_grade = min(grades)
+            average_grade = sum(grades) / len(grades)
+            max_grade = max(grades)
+            file.write(f'{student_name}:{min_grade}, {average_grade:.2f}, {max_grade}\n')
+            
