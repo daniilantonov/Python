82a649c7e5f453fa2a8a1353be93ab2ac74a7b8c
Danil <daniilantonov>
2023-03-05 01:01:45 +0200

    new

--- Student_grades/functions.py
+++ Student_grades/functions.py
@@ -1,5 +1,30 @@
-import script
+import csv
+from collections import defaultdict
 
 
-student_grades=script.reading_student_grades('grades.csv')
-script.save_accumulated_grades(student_grades,'output.csv')
+def reading_student_grades(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        student_grades = defaultdict(list)
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            name = row['student_name']
+            grade = int(row['grade'])
+            student_grades[name].append(grade)
+        return student_grades
+
+def save_accumulated_grades(student_grades, file_name):
+    with open(file_name, 'w', newline='', encoding='utf-8') as file:
+        fieldnames = ['Student Name', 'Min Grade', 'Average Grade', 'Max Grade', 'Number of Grades']
+        csv_writer = csv.DictWriter(file, fieldnames=fieldnames)
+        csv_writer.writeheader()
+        for student_name, grades in student_grades.items():
+            min_grade = min(grades)
+            average_grade = sum(grades) / len(grades)
+            max_grade = max(grades)
+            num_grades = len(grades)
+            csv_writer.writerow({'Student Name': student_name, 
+                                  'Min Grade': min_grade, 
+                                  'Average Grade': f'{average_grade:.2f}', 
+                                  'Max Grade': max_grade, 
+                                  'Number of Grades': num_grades})
+            
--- Student_grades/script.py
+++ Student_grades/script.py
@@ -1,28 +1,5 @@
-import csv
-from collections import defaultdict
+import functions
 
 
-def reading_student_grades(file_path):
-    with open(file_path, 'r', newline='', encoding='utf-8') as csv_file:
-        student_grades = defaultdict(list)
-        reader = csv.reader(csv_file, delimiter=' ')
-        for row in reader:
-            if len(row) != 2:
-                raise Exception('File structure not valid')
-            name = row[0]
-            grade_str = row[1]
-            try:
-                grade = float(grade_str)
-            except ValueError:
-                raise Exception('Grade must be a number')
-            student_grades[name].append(grade)
-    return student_grades
-
-
-def save_accumulated_grades(student_grades, file_name):
-    with open(file_name, 'w', encoding='utf-8') as file:
-        for student_name, grades in student_grades.items():
-            min_grade = min(grades)
-            average_grade = sum(grades) / len(grades)
-            max_grade = max(grades)
-            file.write(f'{student_name}:{min_grade:.2f}, {average_grade:.2f}, {max_grade:.2f}\n')
+student_grades = functions.reading_student_grades('grades.csv')
+functions.save_accumulated_grades(student_grades,'output.csv')
