e033c6ed8d66effee3dc96381740b3e1ffe2e1ef
Danil <daniilantonov>
2023-03-05 01:05:13 +0200

    new

--- Student_grades/script.py
+++ Student_grades/script.py
@@ -2,4 +2,4 @@ import functions
 
 
 student_grades = functions.reading_student_grades('grades.csv')
-functions.save_accumulated_grades(student_grades,'output.csv')
+functions.save_accumulated_grades(student_grades, 'output.csv')
