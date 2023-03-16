a7bc9d775a71915011277daf4de71509de7b29c0
Danil <daniilantonov>
2023-03-16 15:11:17 +0200

    new

--- Student_grades/functions.py
+++ Student_grades/functions.py
@@ -1,30 +1,32 @@
 import csv
-from collections import defaultdict
+from Python.Student_grades.student import Student
 
 
 def reading_student_grades(file_path):
-    with open(file_path, 'r', encoding='utf-8') as file:
-        student_grades = defaultdict(list)
-        csv_reader = csv.DictReader(file)
+    students = []
+    with open(file_path, 'r') as file:
+        csv_reader = csv.reader(file)
+        next(csv_reader)
         for row in csv_reader:
-            name = row['student_name']
-            grade = int(row['grade'])
-            student_grades[name].append(grade)
-        return student_grades
+            name = row[0]
+            grades = [int(grade) for grade in row[1:] if grade.isdigit()]
+            student = Student(name=name, grades=grades)
+            students.append(student)
+    return students
 
-def save_accumulated_grades(student_grades, file_name):
-    with open(file_name, 'w', newline='', encoding='utf-8') as file:
-        fieldnames = ['Student Name', 'Min Grade', 'Average Grade', 'Max Grade', 'Number of Grades']
+
+def save_accumulated_grades(file_path, students):
+    with open(file_path, 'w', newline='') as file:
+        fieldnames = ['Name', 'Max Grade', 'Min Grade', 'Average Grade', 'Number of Grades']
         csv_writer = csv.DictWriter(file, fieldnames=fieldnames)
+        
         csv_writer.writeheader()
-        for student_name, grades in student_grades.items():
-            min_grade = min(grades)
-            average_grade = sum(grades) / len(grades)
-            max_grade = max(grades)
-            num_grades = len(grades)
-            csv_writer.writerow({'Student Name': student_name, 
-                                  'Min Grade': min_grade, 
-                                  'Average Grade': f'{average_grade:.2f}', 
-                                  'Max Grade': max_grade, 
-                                  'Number of Grades': num_grades})
+        for student in students:
+            csv_writer.writerow({
+                'Name': student.get_name(),
+                'Max Grade': student.get_max_grade(),
+                'Min Grade': student.get_min_grade(),
+                'Average Grade': student.get_avg_grade(),
+                'Number of Grades': student.get_num_grades()
+            })
             
--- Student_grades/script.py
+++ Student_grades/script.py
@@ -3,6 +3,3 @@ import homework
 
 students = homework.reading_student_grades('grades.csv')
 homework.save_accumulated_grades('output.csv', students)
-
-# student_grades = homework.reading_student_grades('grades.csv')
-# homework.save_accumulated_grades(students, 'output.csv')
