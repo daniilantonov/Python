fde24d00c21bc92f76735ddf232d0126e0bfb4a3
Danil <daniilantonov>
2023-03-24 17:36:21 +0200

    new

--- Student_grades/student.py
+++ Student_grades/student.py
@@ -1,25 +1,3 @@
-# class Student:
-#     def __init__(self, name, grades):
-#         self.name = name
-#         self.grades = grades
-        
-#     def get_name(self):
-#         return self.name
-
-#     def get_grades(self):
-#         return self.grades
-
-#     def get_max_grade(self):
-#         return max(self.grades)
-
-#     def get_min_grade(self):
-#         return min(self.grades)
-
-#     def get_avg_grade(self):
-#         return sum(self.grades) / len(self.grades)
-
-#     def get_num_grades(self):
-#         return len(self.grades)
 class People:
     def __init__(self, name):
         self.name = name
