3640f7ef64135ab8ffbada3537163cf13edd48db
Danil <daniilantonov>
2023-03-25 12:20:32 +0200

    added_info

--- Student_grades/student.py
+++ Student_grades/student.py
@@ -2,9 +2,6 @@ class People:
     def __init__(self, name):
         self.name = name
 
-    def get_name(self):
-        return self.name
-
     def print_info(self):
         print(f"Name: {self.name}")
 
